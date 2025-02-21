54f2eb1e32edbc3082829a1e550cb907a199ea4e
aytekin789 <aytekinmemmadli@gmail.com>
2025-02-21 15:19:41 +0400

    frontend

--- Dual Mebel/FRONTEND/src/layout/MainLayout.jsx
+++ Dual Mebel/FRONTEND/src/layout/MainLayout.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import MainNavbar from '../Components/Navbar/MainNavbar'
 import { Outlet } from 'react-router-dom'
 import Footer from '../Components/Footer/Footer'
+import Header from '../Components/Header/Header'
 
 const MainLayout = () => {
   return (
    <>
+   <Header/>
    <MainNavbar/>
    <Outlet/>
    <Footer/>
